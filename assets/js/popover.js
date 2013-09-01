bb245b2029ef75770cf638ae7b1f7a3df5a131b1
Rishi Shah <rishihahs@gmail.com>
2013-09-01 17:06:17 -0500

    Popover Alignment

--- assets/js/popover.js
+++ assets/js/popover.js
@@ -2,11 +2,11 @@
     var popover;
 
     var options = {
-        trigger: 'manual',
-        placement: placement,
+        placement: 'auto',
         html: true,
-        title: '<span>mundo (mundo)</span> <button type="button" class="close close-popover">&times;</button>',
-        content: 'Loading...'
+        title: '<span>mundo (mundo)</span>',
+        content: 'Loading...',
+        template: '<div class="popover"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div><button type="button" class="close close-popover">&times;</button></div>'
     };
 
     $('.word').click(function() {
@@ -16,34 +16,35 @@
 
         popover = $(this);
 
-        if (!$(this).data('popover')) {
+        if (!$(this).attr('data-original-title')) {
             $(this).popover(options);
+
+            var self = this;
+            $(this).on('shown.bs.popover', function() {
+                $('.close-popover:not(.bound)').addClass('bound').on('click', function() {
+                    $(self).popover('hide');
+                });
+            });
         }
 
-        $(this).popover('toggle');
-        var self = this;
-        $('.close:not(.bound)').addClass('bound').on('click', function() {
-            $(self).popover('hide');
-        });
+        var $el = $(this).next('.popover');
+
+        if (!$el.is(':visible')) {
+            $(this).popover('show');
+        } else {
+            $(this).popover('hide');
+        }
 
         window.WordReference.getJSON($.trim($(this).text().replace('.', '')), function(content) {console.log(JSON.stringify(content));
             var response;
             if (content.hasOwnProperty('Error')) {
                 response = 'No translation found.';
             } else {
-
+                response = content.term0.PrincipalTranslations[0].FirstTranslation;
             }
+
+            $('.popover-content').html(response);
         });
     });
 
-    function placement() {
-        var position = this.$element.position();
-
-        if (position.top < $(document).height() * 0.10) {
-            return 'bottom';
-        }
-
-        return 'top';
-
-    }
 })(this);
